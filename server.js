7739cb8b49ee9d700cbc869359fff56fdbaa4d5b
Scott Landis <sclandis@merkleinc.com>
2019-09-09 09:48:46 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,101 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var mysql = require('mysql');
+
+var dbConn = mysql.createConnection({
+    host: "mysql.scottclandis.com",
+    user: "vinylrecords",
+    password: "Gordan24",
+    database: "vinyl_records"
+});
+
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
+
+
+// default route
+app.get('/', function (req, res) {
+    return res.send({
+        error: true,
+        message: 'hello'
+    })
+});
+// connection configurations
+
+// connect to database
+dbConn.connect();
+
+
+// Retrieve all users 
+app.get('/records', function (req, res) {
+    dbConn.query('SELECT * FROM records', function (error, results, fields) {
+        if (error) throw error;
+        return res.send({
+            error: false,
+            data: results,
+            message: 'users list.'
+        });
+    });
+
+});
+
+//Add a new record
+app.post('/records', function (req, res) {
+
+    var records = [
+        req.body.albumName,
+        req.body.artistName,
+        req.body.albumSize,
+
+
+        ];
+    console.log(records)
+    // let dataEncoded = JSON.stringify(records);
+    // console.log(dataEncoded)
+    //    if (!records) {
+    //        return res.status(400).send({
+    //            error: true,
+    //            message: 'Please provide record'
+    //        }); 
+    //    }
+
+    dbConn.query("INSERT INTO records (albumName, artistName, albumSize) VALUES ( ? )", [records],
+        function (error, results) {
+            if (error) throw error;
+            return res.send({
+                error: false,
+                data: results,
+                message: 'New records has been created successfully.'
+            });
+        });
+});
+
+
+// set port
+app.listen(3000, function () {
+    console.log('Node app is running on port 3000');
+});
+
+module.exports = app;
